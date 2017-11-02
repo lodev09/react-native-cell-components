cc3823b444569069a1a3e7ddde9a5c4e52e10ead
Jovanni Lo <lodev09@users.noreply.github.com>
2017-11-03 04:44:12 +0800

    tab size

--- components/CellSlider.js
+++ components/CellSlider.js
@@ -4,35 +4,35 @@ import Cell from './Cell';
 import theme from '../lib/theme';
 
 import {
-	Slider,
-	View,
-	Text,
-	StyleSheet
+  Slider,
+  View,
+  Text,
+  StyleSheet
 } from 'react-native';
 
 class CellSlider extends React.Component {
 
-	render() {
-		return (
-			<Cell
-				icon={this.props.icon}
-				title={this.props.title}
-				subtitle={this.props.subtitle}
-				disclosure={this.props.disclosure}
-			>
-				<View style={styles.container}>
-					<Slider style={{ flex: 1 }} {...this.props} />
-				</View>
-			</Cell>
-		);
-	}
+  render() {
+    return (
+      <Cell
+        icon={this.props.icon}
+        title={this.props.title}
+        subtitle={this.props.subtitle}
+        disclosure={this.props.disclosure}
+      >
+        <View style={styles.container}>
+          <Slider style={{ flex: 1 }} {...this.props} />
+        </View>
+      </Cell>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: 'row'
-	}
+  container: {
+    flex: 1,
+    flexDirection: 'row'
+  }
 });
 
 export default CellSlider;
--- components/CellSwitch.js
+++ components/CellSwitch.js
@@ -4,23 +4,23 @@ import Cell from './Cell';
 import theme from '../lib/theme';
 
 import {
-	Switch
+  Switch
 } from 'react-native';
 
 class CellSwitch extends React.Component {
 
-	render() {
-		return (
-			<Cell
-				icon={this.props.icon}
-				title={this.props.title}
-				subtitle={this.props.subtitle}
-				disclosure={this.props.disclosure}
-			>
-				<Switch {...this.props} />
-			</Cell>
-		);
-	}
+  render() {
+    return (
+      <Cell
+        icon={this.props.icon}
+        title={this.props.title}
+        subtitle={this.props.subtitle}
+        disclosure={this.props.disclosure}
+      >
+        <Switch {...this.props} />
+      </Cell>
+    );
+  }
 }
 
 export default CellSwitch;
